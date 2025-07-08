2d354cf34b7496d90d998792287b5e8ea9d3fb12
fabiofumiowada <=>
2025-07-08 11:31:26 -0300

    Atualização do Marital Status

--- apoio/sql_server_tabela_padronizada.sql
+++ apoio/sql_server_tabela_padronizada.sql
@@ -3,18 +3,20 @@
 -- Tabela dataset_employess (Original)
 ALTER TABLE dataset_employess ADD ID INT IDENTITY(1,1)
 GO
--- Em seguida, defina a coluna como chave primária
+-- Em seguida, defina a coluna como chave primï¿½ria
 ALTER TABLE dataset_employess ADD CONSTRAINT PK_dataset_employess PRIMARY KEY (ID);
 GO
 
 -------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------
+
+-- Contra Prova
+
 SELECT count(*)
 FROM dbo.dataset_employess
 
 sp_help dataset_employess
 
---Attrition - Regra de 3
 select	Attrition, 
 		count(*) quantidade, 
 		(count(*)*100)/(SELECT count(*) FROM dbo.dataset_employess) percentual
@@ -59,12 +61,12 @@ from dbo.dataset_employess
 group by BusinessTravel
 
 
-	-- Tabela HR (Dataset não utilizado)
+	-- Tabela HR (Dataset nï¿½o utilizado)
 	/*
 	-- Primeiro, adicione a coluna com propriedade IDENTITY
 	ALTER TABLE HR ADD ID INT IDENTITY(1,1)
 	GO
-	-- Em seguida, defina a coluna como chave primária
+	-- Em seguida, defina a coluna como chave primï¿½ria
 	ALTER TABLE HR ADD CONSTRAINT PK_HR PRIMARY KEY (ID);
 	GO
 	*/
